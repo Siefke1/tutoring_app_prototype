32f9e1bfa1a6bb1273433b39f92988917ec5ac65
Leon Langhoff <leon.langhoff@posteo.de>
2022-05-21 14:51:47 +0200

    bootstrap 5 importmap setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// Bootstrap JS config
+import "popper"
+import "bootstrap"
